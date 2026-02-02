b8006e7cdf56841c3f155c26251250d5ec5bbdfe
MrcsFranca <francamarcos035@gmail.com>
2026-02-02 14:45:01 -0300

    fix: change ip address

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_security_group" "website_sg" {
 
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   security_group_id = aws_security_group.website_sg.id
-  cidr_ipv4         = "177.22.219.163/32"
+  cidr_ipv4         = "177.22.221.181/32"
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
